ef4316cee6b2e0c27af7692088bad8a218084af3
Gani25 <memonabdulgani3d@gmail.com>
2025-07-20 13:19:04 +0530

    One To One bi Directional

--- src/main/java/com/sprk/one_to_one/entity/Student.java
+++ src/main/java/com/sprk/one_to_one/entity/Student.java
@@ -1,10 +1,12 @@
 package com.sprk.one_to_one.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+
 public class Student {
 
     @Id
@@ -20,5 +22,6 @@ public class Student {
     // Aggregation
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name ="student_detail_id" )
+    @JsonManagedReference
     private StudentDetail studentDetail;
 }
--- src/main/java/com/sprk/one_to_one/entity/StudentDetail.java
+++ src/main/java/com/sprk/one_to_one/entity/StudentDetail.java
@@ -1,9 +1,8 @@
 package com.sprk.one_to_one.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -17,4 +16,9 @@ public class StudentDetail {
     private String hobby;
 
     private String education;
+
+    // Bi Directional
+    @OneToOne(mappedBy = "studentDetail")
+    @JsonBackReference
+    private Student student;
 }
